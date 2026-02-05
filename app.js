a9803072eeed71c849bd761c814dc54e412de252
Tina Ostrander <tostrander@greenriver.edu>
2026-02-05 14:25:29 -0800

    Add routing

---
+++ app.js
@@ -0,0 +1,59 @@
+import express from 'express';
+const app = express();
+const PORT = 3000;
+app.use(express.static('public'));
+
+// "Middleware" that allows express to read
+// form data and store it in req.body
+app.use(express.urlencoded({ extended: true }));
+
+// Create a temp array to store orders
+const orders = []; 
+
+// Default route
+app.get('/', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/home.html`);
+});
+
+// Contact route
+app.get('/contact-us', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/contact.html`);
+});
+
+// Confirmation route
+app.get('/thank-you', (req, res) => {
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+// Admin route
+app.get('/admin', (req, res) => {
+    res.send(orders);
+});
+
+// Submit order route
+// {"fname":"a","lname":"aa","email":"a",
+// "method":"delivery","toppings":["artichokes"],
+// "size":"small","comment":"","discount":"on"}
+app.post('/submit-order', (req, res) => {
+    
+    // Create a JSON object to store the order data
+    const order = {
+        fname: req.body.fname,
+        lname: req.body.lname,
+        email: req.body.email,
+        method: req.body.method,
+        toppings: req.body.toppings ? req.body.toppings : "none",
+        size: req.body.size,
+        comment: req.body.comment,
+        timestamp: new Date()
+    };
+
+    // Add order object to orders array
+    orders.push(order);
+
+    res.sendFile(`${import.meta.dirname}/views/confirmation.html`);
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+});
